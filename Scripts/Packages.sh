01b0689dcc8462f21606e1216f7eafa9c39398e0
ichingdionysus <77834916+ichingdionysus@users.noreply.github.com>
2025-02-11 13:46:24 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -31,19 +31,6 @@ UPDATE_VERSION "dockerd"
 
 
 
-# 更新插件列表
-
-# 更新Aria2插件
-UPDATE_PACKAGE "luci-app-aria2" "ningxiaopeng/luci-app-aria2" "master"
-
-# 更新Alist插件
-UPDATE_PACKAGE "alist" "alist-org/alist" "main" "pkg"
-UPDATE_PACKAGE "luci-app-alist" "sbwml/luci-app-alist" "main"
-
-
-
-# 更新AdGuardHome插件
-UPDATE_PACKAGE "luci-app-adguardhome" "rufengsuixing/luci-app-adguardhome" "master"
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 
